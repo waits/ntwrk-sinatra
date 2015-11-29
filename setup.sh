8db1bb1f7f0bdb76c1e1027078ae7467694ca125
Dylan Waits <dylan@waits.io>
2015-11-28 17:37:35 -0800

    Create setup script
    Downloads GeoIP database and generates a 128 megabyte test file

--- setup.sh
+++ setup.sh
@@ -7,4 +7,14 @@ fi
 wget "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
 gunzip "GeoLite2-City.mmdb.gz"
 
+if [ ! -e public/data ]
+  then
+    mkdir public/data
+fi
+
+if [ ! -e public/data/128mb ]
+  then
+    dd if=/dev/zero of=public/data/128mb count=128 bs=1000000
+fi
+
 exit 0
