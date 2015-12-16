c2d878cbbdac93dec40e671b7817af3a36014440
Dylan Waits <dylan@waits.io>
2015-12-16 10:10:04 -0800

    Clean up setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
-#! /bin/sh
+#!/bin/sh
 
-if [ -e GeoLite2-City.mmdb ]
-  then
+if [ -e GeoLite2-City.mmdb ]; then
     rm GeoLite2-City.mmdb
 fi
 wget "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
 gunzip "GeoLite2-City.mmdb.gz"
 
-if [ ! -e public/test ]
-  then
+if [ ! -e public/test ]; then
     mkdir public/test
 fi
 
-if [ ! -e public/test/download ]
-  then
+if [ ! -e public/test/download ]; then
     dd if=/dev/zero of=public/test/download count=128 bs=1000000
 fi
 
